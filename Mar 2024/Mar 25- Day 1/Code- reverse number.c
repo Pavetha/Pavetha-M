cfa548355b37aa4815e3ddc76182c209cfe8661f
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-03-25 23:24:29 +0530

    Create Code- reverse number.c

---
+++ Mar 2024/Mar 25- Day 1/Code- reverse number.c
@@ -0,0 +1,19 @@
+program to reverse a number
+
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        i=n%10;
+        printf("%d",i);
+        n=n/10;
+    }
+    return 0;
+}
+
+output:
+456
+654
