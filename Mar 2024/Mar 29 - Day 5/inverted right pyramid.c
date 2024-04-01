73190387412cb83fcf462e24f41068e6e32ed8b5
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-04-01 19:25:18 +0530

    Create inverted right pyramid.c

---
+++ Mar 2024/Mar 29 - Day 5/inverted right pyramid.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;--i)
+   {
+       for(j=1;j<=n-i;++j)
+       {
+           printf(" ");
+       }
+       for(k=1;k<=i;++k)
+       {
+           printf("*");
+       }
+       printf("\n");
+   }
+   return 0;
+}
+/*
+Output:
+5
+*****
+ ****
+  ***
+   **
+    *     */
